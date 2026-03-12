e9ae4f9f7bd9dc0d54c645844443378a57ccfb18
AstroTheSaint <j@jrapp.org>
2026-03-13 00:56:02 +0700

    reset repo to latest version

--- check_api_empty.js
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-
-async function run() {
-  const shows = await fetch("http://localhost:3000/api/tour//shows").then(r => r.text());
-  console.log("shows", shows);
-}
-run();
