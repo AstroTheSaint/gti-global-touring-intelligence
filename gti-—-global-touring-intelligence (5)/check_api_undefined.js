dc890df3be03510c801e40a708b26aa7a1d2d6b8
AstroTheSaint <j@jrapp.org>
2026-03-13 13:44:07 +0700

    remove incorrectly nested folder

--- gti-—-global-touring-intelligence (5)/check_api_undefined.js
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-
-async function run() {
-  const summary = await fetch("http://localhost:3000/api/tour/undefined/summary").then(r => r.text());
-  console.log("summary", summary);
-}
-run();
--- gti-—-global-touring-intelligence (5)/check_db.js
+++
@@ -1,5 +0,0 @@
-import Database from "better-sqlite3";
-const db = new Database("gti.db");
-console.log(db.prepare("SELECT * FROM artists").all());
-console.log(db.prepare("SELECT * FROM tours").all());
-console.log(db.prepare("SELECT * FROM shows").all());
